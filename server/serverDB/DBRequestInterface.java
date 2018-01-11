c1cfa31d0deaf984b3754db87969a33c441bed7b
Rain-Alexandra BEGG <rainalex01@gmail.com>
2018-01-10 19:54:12 -0500

    getGroups and getGroupTickets methods added

--- server/serverDB/DBRequestInterface.java
+++ server/serverDB/DBRequestInterface.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public interface DBRequestInterface {
 	public ArrayList<String> getGroups(int idPerson);
 	public ArrayList<String> getGroupTickets(int idPerson, String groupName);
